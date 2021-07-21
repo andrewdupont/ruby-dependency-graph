a9b53e2c25c8849d80a50ba1b7a87f915d8a4218
Andrew Dupont <andrew@andrewdupont.dev>
2021-07-21 02:17:30 -0600

    go routines

--- backend/api.go
+++ backend/api.go
@@ -6,10 +6,11 @@ import (
 	"github.com/tidwall/gjson"
 	"io"
 	"net/http"
+	"time"
 	"sync"
 )
 
-func requestGem(gem string) string {
+func requestGem(gem string, retry int) string {
 	url := fmt.Sprintf("https://rubygems.org/api/v1/gems/%v", gem)
 	// fmt.Println(url)
 	req, err := http.NewRequest("GET", url, nil)
@@ -18,6 +19,17 @@ func requestGem(gem string) string {
 	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
+	}
+	// sleep and retry on rate limited
+	if resp.StatusCode == 429 {
+		time.Sleep(1 * time.Second)
+		retry++
+		fmt.Printf("retry to request gem: %s %v times\n", gem, retry)
+		if retry > 5 {
+			panic(resp.Status)
+		}
+
+		requestGem(gem, retry)
 	}
 	if resp.StatusCode != 200 {
 		panic(resp.Status)
@@ -54,54 +66,74 @@ func (m *NodeSet) add(id int, nodeName string) (bool, Node) {
 	defer m.Unlock()
 	_, ok := m.nodes[nodeName]
 	if !ok {
-			newNode := Node{id, nodeName}
-			m.nodes[nodeName] = newNode
-			return true, newNode
+		fmt.Printf("added: %v\n", nodeName)
+		newNode := Node{id, nodeName}
+		m.nodes[nodeName] = newNode
+		return true, newNode
 	}
 	return false, m.nodes[nodeName]
 }
 
-func (nodesSet *NodeSet) getValues() []Node {
+func (nodeSet *NodeSet) getValues() []Node {
 	nodes := []Node{}
-	for _, node := range nodesSet.nodes {
+	for _, node := range nodeSet.nodes {
 		nodes = append(nodes, node)
 	}
 	return nodes
 }
 
-func dependencyLayer(nodesSet NodeSet, links []Link, curNode Node) (NodeSet, []Link) {
-	resp := requestGem(curNode.Name)
+type LinkSet struct {
+	links map[Link]bool
+}
+
+func (m *LinkSet) add(link Link) {
+	m.links[link] = true
+}
+
+func (linkSet *LinkSet) getValues() []Link {
+	links := []Link{}
+	for link := range linkSet.links {
+		links = append(links, link)
+	}
+	return links
+}
+
+func dependencyLayer(wg *sync.WaitGroup, nodeSet NodeSet, linkSet LinkSet, curNode Node) {
+	defer wg.Done()
+	resp := requestGem(curNode.Name, 0)
 	deps := gjson.Get(resp, "dependencies.runtime").Array()
 	for _, dependency := range deps {
-		gemName := gjson.Get(dependency.String(), "name").String()
-		// Guard, this node has already been found.
-		// still add the link to the slice
-		newID := len(nodesSet.nodes)
-		added, node := nodesSet.add(newID, gemName)
-		if !added {
-			newLink := Link{curNode.Id, node.Id}
-			links = append(links, newLink)
-		}
 
+		dependencyName := gjson.Get(dependency.String(), "name").String()
+		newID := len(nodeSet.nodes)
+		added, newNode := nodeSet.add(newID, dependencyName)
+		newLink := Link{curNode.Id, newNode.Id}
+		linkSet.add(newLink)
+
+		if !added {
+			continue
+		}
+		wg.Add(1)
 		// Recursively find the next layer of dependencies
-		go dependencyLayer(nodesSet, links, node)
+		go dependencyLayer(wg, nodeSet, linkSet, newNode)
 	}
-	return nodesSet, links
 }
 
 // https://stackoverflow.com/questions/27103161/recursive-goroutines-what-is-the-neatest-way-to-tell-go-to-stop-reading-from-ch
 func DependencyGraph(gem string) (string, string) {
-	var nodesSet = NodeSet{nodes: make(map[string]Node)}
-	var links = []Link{}
-	_, node := nodesSet.add(0, gem)
-	nodesResults, linkResults := dependencyLayer(nodesSet, links, node)
+	var nodeSet = NodeSet{nodes: make(map[string]Node)}
+	_, node := nodeSet.add(0, gem)
+	var linkSet = LinkSet{links: make(map[Link]bool)}
+	var wg sync.WaitGroup
+	wg.Add(1)
+	dependencyLayer(&wg, nodeSet, linkSet, node)
+	wg.Wait()
 
-	nodesJSON, err := json.Marshal(nodesResults.getValues())
+	nodesJSON, err := json.Marshal(nodeSet.getValues())
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	linksJSON, err := json.Marshal(linkResults)
+	linksJSON, err := json.Marshal(linkSet.getValues())
 	if err != nil {
 		fmt.Println(err)
 	}
--- backend/go.mod
+++ backend/go.mod
@@ -1,4 +1,4 @@
-module test_http
+module rubygems_deps_app
 
 go 1.16
 
